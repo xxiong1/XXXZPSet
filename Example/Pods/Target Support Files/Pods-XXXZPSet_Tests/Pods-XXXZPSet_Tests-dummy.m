368d0b8753ad94da77f948c1fe4934cd521abe48
xxiong1 <13168376064@163.com>
2019-08-20 12:23:57 +0800

    创建

---
+++ Example/Pods/Target Support Files/Pods-XXXZPSet_Tests/Pods-XXXZPSet_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XXXZPSet_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XXXZPSet_Tests
+@end
---
+++ Example/Pods/Target Support Files/XXXZPSet/XXXZPSet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XXXZPSet : NSObject
+@end
+@implementation PodsDummy_XXXZPSet
+@end
